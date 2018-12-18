fa4283046fd7f92aeab2f9c20f8dedc6618b25d6
henrys <henrys@mustardsystems.com>
2018-12-18 17:39:49 +0000

    updated parameters post optimiser

--- params/player_assist_params.py
+++ params/player_assist_params.py
@@ -1,8 +1,10 @@
 player_assist_params = {
-	"player_assist_P0": 0.0359,
-	"player_assist_Q": 0.150,
-	"player_assist_x0_att": 0.112,
-	"player_assist_x0_def": 0.0481,
-	"player_assist_x0_gks": 0.00326,
-	"player_assist_x0_mid": 0.117,
+	"player_assist_P0": 0.0498,
+	"player_assist_Q": 0.000725,
+	"player_assist_x0_att": 0.120,
+	"player_assist_x0_def": 0.0571,
+	"player_assist_x0_gks": 0.00238,
+	"player_assist_x0_mid": 0.121,
+
 }
+
--- params/player_goal_params.py
+++ params/player_goal_params.py
@@ -1,8 +1,8 @@
 player_goal_params = {
-	"player_goal_P0": 0.0412,
-	"player_goal_Q": 2.23e-07,
-	"player_goal_x0_att": 0.243,
-	"player_goal_x0_def": 0.0359,
-	"player_goal_x0_gks": 0.00330,
-	"player_goal_x0_mid": 0.0839,
+	"player_goal_P0": 0.0462,
+	"player_goal_Q": 0.000764,
+	"player_goal_x0_att": 0.253,
+	"player_goal_x0_def": 0.0411,
+	"player_goal_x0_gks": 0.00623,
+	"player_goal_x0_mid": 0.0844,
 }
